6604bde5a4de96385679eeebab2195395e7be86c
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-21 17:27:29 +0100

    Added in second test for tasks. All tests have passed in todolist

--- spec/ToDoList_spec.rb
+++ spec/ToDoList_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe ToDoList do
       expect(todo_list.tasks.size).to eq(1)
     end
   end
+
+  describe "#tasks" do
+    it "returns all the tasks in the todo list" do
+      todo_list.add_task("Finish report")
+      todo_list.add_task("Buy groceries")
+      expect(todo_list.tasks).to match_array(["Finish report", "Buy groceries"])
+    end
+  end
 end
