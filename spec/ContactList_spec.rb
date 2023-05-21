1dbb991152ea45470226ef4f17b060881ffb356b
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-21 18:57:00 +0100

    Added in final test for contact list. All tests pass

--- spec/ContactList_spec.rb
+++ spec/ContactList_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe ContactList do
       expect(contacts.mobile_numbers.size).to eq(1)
     end
   end
+
+  describe "#mobile_numbers" do
+    it "returns all the mobile numbers in the contact list" do
+      contacts.add_contact("John Doe", "1234567890")
+      contacts.add_contact("Jane Smith", "9876543210")
+      expect(contacts.mobile_numbers).to match_array(["1234567890", "9876543210"])
+    end
+  end
 end
